cb5d76696190bf4abc27406a938fdca8eec99c73
Thomas Astete <thomas.astete@inacapmail.cl>
2025-10-07 16:15:03 -0300

    finalizacion de creacion jugador

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,17 @@
 extends Area2D
+signal hit
 
-@export var speed = 400
-var screen_size
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
 
 func _ready():
 	screen_size = get_viewport_rect().size
-	
+	#hide()
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
@@ -23,5 +29,19 @@ func _process(delta):
 	else:
 		$AnimatedSprite2D.stop()
 		
-		position += velocity * delta
-		position = position.clamp(Vector2.ZERO, screen_size)
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		# See the note below about the following boolean assignment.
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
